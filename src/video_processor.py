27d5a36954d75285bdc91da2d5088bbd43a0b3c6
roger-marcos <108490849+roger-marcos@users.noreply.github.com>
2023-11-28 12:03:17 +0100

    Update video_processor.py

--- src/video_processor.py
+++ src/video_processor.py
@@ -1,71 +1,55 @@
 import cv2
 import numpy as np
+import tensorflow as tf
 
-# Import the emotion detection function from emotion_detector.py
-from emotion_detector import detect_emotion
+# Load the pre-trained emotion detection model
+model = tf.keras.models.load_model('models/emotion_detection_model.h5')
 
-def preprocess_frame(frame):
-    """
-    Apply necessary preprocessing to the video frame
-    before emotion detection. This might include resizing,
-    color adjustments, etc.
-    """
-    # Convert frame to grayscale
-    processed_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    return processed_frame
+def preprocess_image(image, target_size=(48, 48)):
+    """Resizes and scales the image for model prediction."""
+    if len(image.shape) == 2 or image.shape[2] == 1:
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    image = cv2.resize(image, target_size)
+    image = image.astype('float32') / 255  # Normalize
+    return np.expand_dims(image, axis=0)
 
-def face_detection(frame):
-    """
-    Detect faces in the given frame using Haar Cascade Classifier.
-    Returns the coordinates of detected faces.
-    """
-    # Load the Haar Cascade Classifier for face detection
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-    # Detect faces in the frame
-    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    return faces
+def predict_emotion(image):
+    """Predicts the emotion from the given image."""
+    processed_image = preprocess_image(image)
+    emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+    prediction = model.predict(processed_image)
+    return emotions[np.argmax(prediction)]
 
 def process_video_stream():
-    """
-    Capture video from the webcam and process each frame
-    for emotion detection.
-    """
-    # Initialize webcam
+    # Load Haar Cascade for face detection
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    # Initialize Webcam
     cap = cv2.VideoCapture(0)
 
     while True:
-        # Read frame from webcam
         ret, frame = cap.read()
         if not ret:
             break
 
-        # Preprocess the frame
-        processed_frame = preprocess_frame(frame)
+        # Convert to grayscale
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Detect faces in the frame
-        faces = face_detection(processed_frame)
+        # Detect faces
+        faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
+        # Process each face and display emotion
         for (x, y, w, h) in faces:
-            # Extract the face from the frame
-            face = processed_frame[y:y+h, x:x+w]
-
-            # Detect emotion for the extracted face
-            emotion = detect_emotion(face)
-
-            # Display the detected emotion
+            face_img = gray_frame[y:y+h, x:x+w]
+            emotion = predict_emotion(face_img)
             cv2.putText(frame, emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            # Draw a rectangle around the face
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-        # Display the resulting frame
-        cv2.imshow('Video Stream', frame)
+        cv2.imshow('Real-Time Emotion Detection', frame)
 
-        # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Release the webcam and close all windows
     cap.release()
     cv2.destroyAllWindows()
 
